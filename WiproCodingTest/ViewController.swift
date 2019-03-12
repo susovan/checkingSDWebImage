4f7f444f1da34190796eff58583d4dbea4c9c75b
Susovan - <su20072929@bhargavas-macbook-pro.local>
2019-03-12 20:24:07 +0530

    Initial Commit

---
+++ WiproCodingTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WiproCodingTest
+//
+//  Created by Susovan Pati - (Digital) on 12/03/19.
+//  Copyright © 2019 Wipro Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
